fea52ee92d47ff78b16404de1d14793055ebc78b
Conor Kincart <conor.kincart1@marist.edu>
2014-02-12 12:31:12 -0500

    Lab 3
    Conor Kincart
    February 7, 2014
    Lab 3

---
+++ lab3/lab3.sql
@@ -0,0 +1,39 @@
+﻿select name, city
+from agents 
+where name = 'Smith';
+
+select pid, name, quantity
+from products
+where priceUSD > 1.25;
+
+select ordno, aid
+from orders;
+
+select name, city 
+from customers
+where city = 'Dallas';
+
+select name 
+from agents
+where city != 'New York'
+  and city != 'Newark';
+
+select * 
+from products
+where city != 'New York'
+  or city != 'Newark'
+  and priceUSD >= 1;
+
+select *
+from orders
+where mon = 'jan'
+or mon = 'mar';
+
+select *
+from orders 
+where mon = 'feb'
+and dollars < 100;
+
+select *
+from orders 
+where cid = 'c001';
